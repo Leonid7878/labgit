057c18cc0a410c78bfea9b96f1b0c343f72a693f
Leonid <llleeoo22000055@gmail.com>
2024-12-26 01:44:05 +0300

    зафиксировал деление в ветке

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 int main() {
     int a, b;
-    cout << "Enter A and B: ";
+    cout << "Введите A и B: ";
     cin >> a >> b;
     
     cout << "A + B = " << a + b << '\n';
-    cout << "A * B = " << a * b << '\n';  // Добавили вывод произведения
+    if (b != 0) {
+        cout << "A / B = " << a / b << '\n';  // Заменили умножение на деление
+    } else {
+        cout << "Деление на ноль невозможно.\n";  // Обработали деление на ноль
+    }
 
     return 0;
 }
