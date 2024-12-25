6541c7bd6ed5c76123cdf63cebea45c5a9d471d5
Leonid <llleeoo22000055@gmail.com>
2024-12-25 22:35:16 +0300

    Добавлен вывод суммы чисел.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
     int a, b;
+
+    // Ввод двух чисел
     cout << "Введите два числа: ";
     cin >> a >> b;
+
+    // Вывод суммы чисел
+    cout << "Сумма чисел: " << (a + b) << endl;
+
     return 0;
 }
